3656665eb6d40e6019b02f062960db5a7d59bdc4
GermainZ <germanosz@gmail.com>
2018-12-14 16:00:13 +0100

    Fix code alignment

--- web/client/main.js
+++ web/client/main.js
@@ -48,22 +48,22 @@ function onStationClick(station, e) {
     }).then(function(){
         fetch(new URL(wikiurl)).then(async function(response) {
 
-        return response.json();
-    }).then(function(resp) {
+            return response.json();
+        }).then(function(resp) {
 
-        if ('extract' in resp) {
-            content += '<h2>Summary:</h2><p>';
-            for (let descr of resp.extract)
-                content +=  descr ;
+            if ('extract' in resp) {
+                content += '<h2>Summary:</h2><p>';
+                for (let descr of resp.extract)
+                    content +=  descr ;
 
-            content += '</p>';
-        }
+                content += '</p>';
+            }
 
-        wikiurl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
-        popup.setContent(template());
-        popup.update();
+            wikiurl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
+            popup.setContent(template());
+            popup.update();
 
-    });
+        });
     });
 
 
@@ -73,66 +73,66 @@ function onStationClick(station, e) {
 
 // Source: <https://stackoverflow.com/a/321527/1460652>
 function partial(func /*, 0..n args */) {
-  var args = Array.prototype.slice.call(arguments, 1);
-  return function() {
-    var allArguments = args.concat(Array.prototype.slice.call(arguments));
-    return func.apply(this, allArguments);
-  };
+    var args = Array.prototype.slice.call(arguments, 1);
+    return function() {
+        var allArguments = args.concat(Array.prototype.slice.call(arguments));
+        return func.apply(this, allArguments);
+    };
 }
 
 var data = france;
 function setupMap() {
-  //  var map = L.map('map').setView([46, 2], 6);
-  //var map = new L.Map('map', {zoom: 6, center: new L.latLng([46, 2]) });
-  // create a red polygon from an array of LatLng points
-  L.Mask = L.Polygon.extend({
-  	options: {
-  		stroke: false,
-  		color: '#000',
-  		fillOpacity: 1,
-  		clickable: true,
+    //  var map = L.map('map').setView([46, 2], 6);
+    //var map = new L.Map('map', {zoom: 6, center: new L.latLng([46, 2]) });
+    // create a red polygon from an array of LatLng points
+    L.Mask = L.Polygon.extend({
+        options: {
+            stroke: false,
+            color: '#000',
+            fillOpacity: 1,
+            clickable: true,
 
-  		outerBounds: new L.LatLngBounds([-90, -360], [90, 360])
-  	},
+            outerBounds: new L.LatLngBounds([-90, -360], [90, 360])
+        },
 
-  	initialize: function (latLngs, options) {
+        initialize: function (latLngs, options) {
 
-           var outerBoundsLatLngs = [
-  			this.options.outerBounds.getSouthWest(),
-  			this.options.outerBounds.getNorthWest(),
-  			this.options.outerBounds.getNorthEast(),
-  			this.options.outerBounds.getSouthEast()
-  		];
-          L.Polygon.prototype.initialize.call(this, [outerBoundsLatLngs, latLngs], options);
-  	},
+            var outerBoundsLatLngs = [
+                this.options.outerBounds.getSouthWest(),
+                this.options.outerBounds.getNorthWest(),
+                this.options.outerBounds.getNorthEast(),
+                this.options.outerBounds.getSouthEast()
+            ];
+            L.Polygon.prototype.initialize.call(this, [outerBoundsLatLngs, latLngs], options);
+        },
 
-  });
-  L.mask = function (latLngs, options) {
-  	return new L.Mask(latLngs, options);
-  };
+    });
+    L.mask = function (latLngs, options) {
+        return new L.Mask(latLngs, options);
+    };
 
 
-  var lat = 46.566414;
-  var lng =  2.4609375;
-  var zoom =  6;
+    var lat = 46.566414;
+    var lng =  2.4609375;
+    var zoom =  6;
 
-  var map = new L.Map('map');
+    var map = new L.Map('map');
 
-  var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-  var osmAttrib='Map data &copy; OpenStreetMap contributors';
-  var osm = new L.TileLayer(osmUrl, {minZoom: 3, maxZoom: 8, attribution: osmAttrib});
-  map.addLayer(osm);
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data &copy; OpenStreetMap contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 3, maxZoom: 8, attribution: osmAttrib});
+    map.addLayer(osm);
 
-  map.setView(new L.LatLng(lat, lng), zoom);
+    map.setView(new L.LatLng(lat, lng), zoom);
 
-  // transform geojson coordinates into an array of L.LatLng
-  var coordinates = data.features[0].geometry.coordinates[0];
-  var latLngs = [];
-  for (i=0; i<coordinates.length; i++) {
-      latLngs.push(new L.LatLng(coordinates[i][1], coordinates[i][0]));
-  }
+    // transform geojson coordinates into an array of L.LatLng
+    var coordinates = data.features[0].geometry.coordinates[0];
+    var latLngs = [];
+    for (i=0; i<coordinates.length; i++) {
+        latLngs.push(new L.LatLng(coordinates[i][1], coordinates[i][0]));
+    }
 
-  L.mask(latLngs).addTo(map);
+    L.mask(latLngs).addTo(map);
 
 
     L.tileLayer('https://api.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -170,66 +170,66 @@ function setupMap() {
 
     // add a layer group, yet empty
 
-  //  var markersLayer = new L.LayerGroup();
-  //  map.addLayer(markersLayer);
+    //  var markersLayer = new L.LayerGroup();
+    //  map.addLayer(markersLayer);
 
-  var featuresLayer = new L.GeoJSON(data, {
-      style: {
-          opacity: 0,
-          fillOpacity: 0,
-      }
+    var featuresLayer = new L.GeoJSON(data, {
+        style: {
+            opacity: 0,
+            fillOpacity: 0,
+        }
     });
 
-  map.addLayer(featuresLayer);
+    map.addLayer(featuresLayer);
 
     // add the search bar to the map
     var controlSearch = new L.Control.Search({
-    position:'topleft',    // where do you want the search bar?
-    layer: featuresLayer,
-    propertyName: 'nom',
-    initial: false,
-    marker: false,
-    textPlaceholder: 'Departure departement ', // placeholder while nothing is searched
-    collapsed : false,
-    moveToLocation: function(latlng, title, map) {
-    		//map.fitBounds( latlng.layer.getBounds() );
-    		var zoom = map.getBoundsZoom(latlng.layer.getBounds());
-  			map.setView(latlng, zoom); // access the zoom
-    	}
-    //sourceData : '../../data/_stations.json'
-  });
+        position:'topleft',    // where do you want the search bar?
+        layer: featuresLayer,
+        propertyName: 'nom',
+        initial: false,
+        marker: false,
+        textPlaceholder: 'Departure departement ', // placeholder while nothing is searched
+        collapsed : false,
+        moveToLocation: function(latlng, title, map) {
+            //map.fitBounds( latlng.layer.getBounds() );
+            var zoom = map.getBoundsZoom(latlng.layer.getBounds());
+            map.setView(latlng, zoom); // access the zoom
+        }
+        //sourceData : '../../data/_stations.json'
+    });
 
-  controlSearch.on('search:locationfound', function(e) {
+    controlSearch.on('search:locationfound', function(e) {
 
-    	console.log('search:locationfound', );
+        console.log('search:locationfound', );
 
-    	//map.removeLayer(this._markerSearch)
+        //map.removeLayer(this._markerSearch)
 
-    	e.layer.setStyle({opacity: 1});
-    	if(e.layer._popup)
-    		e.layer.openPopup();
+        e.layer.setStyle({opacity: 1});
+        if(e.layer._popup)
+            e.layer.openPopup();
 
     }).on('search:collapsed', function(e) {
 
-    	featuresLayer.eachLayer(function(layer) {	//restore feature color
-    		featuresLayer.resetStyle(layer);
-    	});
+        featuresLayer.eachLayer(function(layer) {	//restore feature color
+            featuresLayer.resetStyle(layer);
+        });
     });
 
 
-  map.addControl(controlSearch); // add it to the map
+    map.addControl(controlSearch); // add it to the map
 
-  // create the control
-  var command = L.control({position: 'topright'});
+    // create the control
+    var command = L.control({position: 'topright'});
 
-  command.onAdd = function (map) {
-      var div = L.DomUtil.create('div', 'command');
+    command.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'command');
 
-      div.innerHTML = '<form id="price"><input id="my-custom-control" type="number" value="0" min="0"/>Price</form>';
-      return div;
-  };
+        div.innerHTML = '<form id="price"><input id="my-custom-control" type="number" value="0" min="0"/>Price</form>';
+        return div;
+    };
 
-  command.addTo(map);
+    command.addTo(map);
 
     // Testing: show paths test.
     loadTest(map);
